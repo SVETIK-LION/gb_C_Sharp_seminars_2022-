56a22aca03a3558c329c51ecc7f793b8ac1b2d2c
Svetlana Lvovskaya <svetlana.lvovskaya@gmail.com>
2022-11-09 15:41:56 +0500

    task_2 is completed

---
+++ task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход число
+и выдаёт сумму цифр в числе.
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */
+
+
+int SumNumbersDigits(int number)
+{
+    string str_number = number.ToString();
+    int result = 0;
+    for (int i = 0; i < str_number.Length; i++)
+    {
+        result += number % 10;
+        number /= 10;
+
+    }
+    return result;
+}
+
+
+Console.WriteLine(SumNumbersDigits(452));
